e144bb5a0f421edb437e3a64268e4aac96c61ad2
dbortoluzzi <danieleb88@gmail.com>
2022-04-16 10:52:05 +0200

    fix aif json

--- src/main/java/org/mpai/store/coapserver/App.java
+++ src/main/java/org/mpai/store/coapserver/App.java
@@ -21,7 +21,7 @@ public class App
     @Bean
     CoapServer coapServer() throws IOException {
         CoapServer coapServer = newBuilder().transport(InetAddress.getByName(mpaiStoreHost), 5683).build();
-        coapServer.addRequestHandler("/test", new ReadOnlyCoapResource("SONO VIVO"));
+        coapServer.addRequestHandler("/test", new ReadOnlyCoapResource("Hello from coap server"));
         coapServer.addRequestHandler("/large", new ReadOnlyCoapResource("{\"$schema\":\"https://json-schema.org/draft/2020-12/schema\",\"$id\":\"https://mpai.community/standards/resources/MPAI-AIF/V1/AIF-metadata.schema.json\",\"title\":\"MPAI-AIF V1 AIF metadata\",\"ImplementerID\":1,\"Version\":\"v1.0\",\"APIProfile\":\"Main\",\"ResourcePolicies\":[{\"Name\":\"Memory\",\"Minimum\":\"50000\",\"Maximum\":\"120000\",\"Request\":\"80000\"},{\"Name\":\"CPUNumber\",\"Minimum\":\"1\",\"Maximum\":\"2\",\"Request\":\"1\"},{\"Name\":\"CPU:Class\",\"Minimum\":\"Low\",\"Maximum\":\"High\",\"Request\":\"Low\"}],\"Authentication\":\"admin\",\"TimeBase\":\"NTP\"}"));
         coapServer.addRequestHandler("/config/aif/demo", new ReadOnlyCoapResource("{\"$schema\":\"https://json-schema.org/draft/2020-12/schema\",\"$id\":\"https://mpai.community/standards/resources/MPAI-AIF/V1/AIF-metadata.schema.json\",\"title\":\"MPAI-AIF V1 AIF metadata\",\"ImplementerID\":1,\"Version\":\"v1.0\",\"APIProfile\":\"Main\",\"ResourcePolicies\":[{\"Name\":\"Memory\",\"Minimum\":\"50000\",\"Maximum\":\"120000\",\"Request\":\"80000\"},{\"Name\":\"CPUNumber\",\"Minimum\":\"1\",\"Maximum\":\"2\",\"Request\":\"1\"},{\"Name\":\"CPU:Class\",\"Minimum\":\"Low\",\"Maximum\":\"High\",\"Request\":\"Low\"}],\"Authentication\":\"admin\",\"TimeBase\":\"NTP\"}"));
 
